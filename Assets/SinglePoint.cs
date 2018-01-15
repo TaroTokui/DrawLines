2bd917634930bdbc8f977d12668881522a9863f6
tokui <tokui@1-10.com>
2018-01-15 17:42:59 +0900

    add single point script

--- Assets/SinglePoint.cs
+++ Assets/SinglePoint.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent (typeof(MeshRenderer))]
-[RequireComponent (typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
 
-public class SingleLine : MonoBehaviour {
+public class SinglePoint : MonoBehaviour {
 
     private void Start()
     {
         var mesh = new Mesh();
         mesh.vertices = new Vector3[] {
-            new Vector3 (0, 1f),
-            new Vector3 (0, -1f),
+            new Vector3 (0, 0),
         };
+        //mesh.normals = new Vector3[] {
+        //    new Vector3 (0, 1.0f, 0),
+        //};
 
         var filter = GetComponent<MeshFilter>();
         filter.sharedMesh = mesh;
@@ -24,9 +26,9 @@ public class SingleLine : MonoBehaviour {
     int[] MakeIndices()
     {
         int[] indices = new int[] {
-            0, 1
+            0
         };
-        
+
         return indices;
     }
 }
